e59964aa9d5a277e5bbd8d42f2cc15c6a0448040
notfreephilly <not.philyoder93@gmail.com>
2026-09-23 13:06:48 -0700

    added scroll feature for shows, included more styling

--- lvl-1/script.js
+++ lvl-1/script.js
@@ -9,8 +9,8 @@ function plusSlides(n) {
 }
 
 // thumbnail image controls
-function currentSlide(s) {
-    showSlides(slideIndex = s);
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
 
@@ -22,12 +22,16 @@ function showSlides(n) {
         slideIndex = 1
     }
 
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += "active";
